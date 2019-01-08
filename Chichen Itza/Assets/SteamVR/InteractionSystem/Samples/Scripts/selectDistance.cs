aa42bdfbdadebc981fc6eb9f0de852be1f268c86
MagicaNexus <rodematthieu@gmail.com>
2019-01-08 18:03:18 +0100

    Ajout Chichen Itza

---
+++ Chichen Itza/Assets/SteamVR/InteractionSystem/Samples/Scripts/selectDistance.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System;
+namespace Valve.VR.InteractionSystem
+{
+    [RequireComponent(typeof(Interactable))]
+    public class selectDistance : MonoBehaviour
+    {
+        public CustomEvents.UnityEventHand onHandClick;
+        private Hand currentHand;
+        public float distance;
+        public GameObject cible;
+        public float damping = 10f;
+
+        private void Update()
+        {
+            cible.transform.position = Vector3.MoveTowards(cible.transform.position, new Vector3(0f, 1.65f, distance), damping * Time.deltaTime);
+        }
+        void Awake()
+        {
+            Button button = GetComponent<Button>();
+            if (button)
+            {
+                button.onClick.AddListener(OnButtonClick);
+            }
+        }
+
+        void Distance(Hand hand)
+        {
+            //cible.transform.position = new Vector3(0f, 1.8f, distance);
+        }
+
+        private void OnButtonClick()
+        {
+            onHandClick.Invoke(currentHand);
+            cible.transform.position = new Vector3(0f, 1.8f, distance);
+        }
+    }
+}
---
+++ Chichen Itza/Assets/TargetScript.cs
@@ -0,0 +1,57 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class TargetScript : MonoBehaviour
+    {
+        public float distance;
+        public Vector3 actualPosition;
+        public Vector3 nextPosition;
+        public Vector3 yPosition;
+
+        public void set10m()
+        {
+            GameObject.Find("Target").transform.position = new Vector3(121f, 1.7f, -3f);
+        }
+
+        public void set15m()
+        {
+            GameObject.Find("Target").transform.position = new Vector3(126f, 1.7f, -3f);
+        }
+
+        public void set20m()
+        {
+            GameObject.Find("Target").transform.position = new Vector3(131f, 1.7f, -3f);
+        }
+
+        public void set30m()
+        {
+            GameObject.Find("Target").transform.position = Vector3.MoveTowards(transform.position, new Vector3(141f, 1.7f, -3f), Time.deltaTime * 2);
+        }
+
+        public void set50m()
+        {
+            GameObject.Find("Target").transform.position = new Vector3(151f, 1.7f, -3f);
+        }
+
+        public void petiteTaille()
+        {
+            GameObject.Find("Target").transform.localScale = new Vector3(50f, 50f, 50f);
+        }
+
+        public void normaleTaille()
+        {
+            GameObject.Find("Target").transform.localScale = new Vector3(100f, 100f, 100f);
+            //GameObject.Find("Target").transform.position.x = 
+        }
+
+        public void grandeTaille()
+        {
+            GameObject.Find("Target").transform.localScale = new Vector3(200f, 200f, 200f);
+        }
+    }
+}
